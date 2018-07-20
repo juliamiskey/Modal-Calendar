acd6ccabf386da7afb17683b790c31bb1ac01683
Julia Miskey <juliamiskey@gmail.com>
2018-07-20 01:44:32 -0400

    initial commit

---
+++ dev/scripts/Calendar.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Calendar extends Component {
+    render() {
+        return (
+
+            <div className="calendar__modal">
+                <div className="calendar__modal__header">
+                    <img className="calendar__header__arrow calendar__header__arrow__left" src="public/styles/assets/left-arrow.png" alt="left arrow" />
+                    <h2 className="calendar__month">May 2018</h2>
+                    <img className="calendar__header__arrow calendar__header__arrow__right" src="public/styles/assets/right-arrow.png" alt="right arrow"/>
+                </div>
+                
+                <div className="calendar">
+                    <div className="calendar__weekdays">
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">M</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">T</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">W</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">T</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">F</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">S</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item calendar__weekdays__day">S</div></div>
+                    </div>
+                    <div className="calendar__body">
+                        <div className="calendar__cell"><div className="calendar__cell__item empty"></div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">1</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">2</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">3</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">4</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">5</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">6</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">7</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">8</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">9</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">10</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">1</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">12</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">13</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">14</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">15</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">16</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">17</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">18</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-unavailable">19</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available date-selected">20</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">21</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">22</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">23</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">24</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">25</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">26</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">27</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">28</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">29</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">30</div></div>
+                        <div className="calendar__cell"><div className="calendar__cell__item date-available">31</div></div>
+                    </div>
+                </div>
+                <div className="calendar__subtext">
+                    <p>Delivery dates will change for all upcoming boxes.</p>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Calendar;
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import Moment from 'react-moment';
+import moment from 'moment';
+import Datetime from 'react-datetime';
+import Calendar from './Calendar';
+
 
 class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {  
+      selectedDate: new Date(),
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.selectDate = this.selectDate.bind(this);
+    // this.defaultDate = this.defaultDate.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  handleCloseModal() {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  selectDate(date) {
+    this.setState({
+      selectedDate: date
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello
+        <h1>Choose your delivery day</h1>
+        <button onClick={this.handleOpenModal}>Pick a Date</button>
+        <Modal
+          isOpen={this.state.showModal}
+          className="modal"
+          overlayClassName="overlay"
+          selectedDate={this.state.selectedDate}
+          handleSelectDate={this.selectDate}
+          onRequestClose={this.handleCloseModal}>
+          
+         <Calendar />
+
+          <button onClick={this.handleCloseModal} className="btn calendar__cancel-btn">Cancel, Don't Change</button>
+          <button onClick={this.handleCloseModal} className="btn calendar__confirm-btn">Change Date</button>
+        </Modal>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const props = {};
+
+ReactDOM.render(<App {...props} />, document.getElementById('app'))
+
+export default App;
